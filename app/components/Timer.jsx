43aca9635bd5010398c11fd9bd7644ccde838282
dan mahavithana <mahathun.online@gmail.com>
2016-12-22 22:52:27 +1300

    Added routes

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render:function(){
+    return (
+      <div>
+        This is the timer compnent
+      </div>
+    );
+  }
+});
+
+module.exports = Timer;
