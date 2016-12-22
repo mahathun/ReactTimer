2cd629e2e51f8a4405497b3051f56795470245f9
dan mahavithana <mahathun.online@gmail.com>
2016-12-23 11:30:54 +1300

    Aded the clock component

---
+++ app/components/Clock.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+
+var Clock = React.createClass({
+
+  getDefaultProps:function(){
+      //return {
+        totalSec: 0
+      //}
+  },
+  formatSeconds:function(totalSec){
+
+    var minuts = Math.floor(totalSec/60);
+    var seconds = totalSec%60;
+
+    minuts =(minuts<10)?"0"+minuts:minuts;
+    seconds =(seconds<10)?"0"+seconds:seconds;
+
+    return (minuts + ":" + seconds);
+  },
+  render: function () {
+    var {totalSec} = this.props;
+
+    return (
+      <div className="clock">
+        <span className="clock-text">
+          {this.formatSeconds(totalSec)}
+        </span>
+      </div>
+    );
+  }
+});
+
+module.exports = Clock;
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
-
+var Clock = require('Clock');
 var Countdown = React.createClass({
   render:function(){
     return (
       <div>
         Countdown
+        <Clock totalSec="62" />
       </div>
     );
   }
